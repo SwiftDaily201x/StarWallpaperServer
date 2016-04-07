e8b302857a6ae21b46b44958a71776b15d4402b5
LuWeijun <luweijun@weidian.com>
2016-04-07 23:06:46 +0800

    bbbb；

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ http.createServer(function (request, response) {
   console.log('aaaa:')
   if (arg.keyword && arg.imgWidth && arg.imgHeight) 
   {
+    console.log('bbbb:')
     var strUrl = 'http://api.lovebizhi.com/iphone_v3.php?a=search&kw='+arg.keyword+'&client_id=1002&model_id=100&screen_width='+arg.imgWidth+'&screen_height='+arg.imgHeight+'&bizhi_width='+arg.imgWidth+'&bizhi_height='+arg.imgHeight
     http.get(strUrl, function(res){
     	res.setEncoding("utf-8")
     	var resData = []
     	res.on("data", function(chunk){
-       	 	resData.push(chunk)
+        console.log('dddd:')
+       	resData.push(chunk)
     	})
     	res.on("end", function(){
+        console.log('eeee:')
         response.writeHead('aaa')
         response.write(resData.toString())
         response.end()
@@ -22,6 +25,7 @@ http.createServer(function (request, response) {
   }	
   else
   {
+    console.log('cccc:')
     response.writeHead('aaa')
     response.write('You need keyword, imgWidth and imgHeight!')
     response.end()
