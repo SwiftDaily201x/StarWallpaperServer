68576a2f6dc97f3a7145cf3ee1bd71e9198b38b5
LuWeijun <luweijun@weidian.com>
2016-04-07 23:01:29 +0800

    aaaa；

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var url = require("url")
 var fs = require('fs')
 http.createServer(function (request, response) {
   var arg = url.parse(request.url,true).query
+  console.log('aaaa:')
   if (arg.keyword && arg.imgWidth && arg.imgHeight) 
   {
     var strUrl = 'http://api.lovebizhi.com/iphone_v3.php?a=search&kw='+arg.keyword+'&client_id=1002&model_id=100&screen_width='+arg.imgWidth+'&screen_height='+arg.imgHeight+'&bizhi_width='+arg.imgWidth+'&bizhi_height='+arg.imgHeight
-	  console.log(strUrl)
     http.get(strUrl, function(res){
     	res.setEncoding("utf-8")
     	var resData = []
