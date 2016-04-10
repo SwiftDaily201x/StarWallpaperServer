e53ee0f50194e2ed3b1c7cd1ccda5059b6813fc2
LuWeijun <luweijun@weidian.com>
2016-04-10 15:28:06 +0800

    坑，node.js中文被截断；

--- server.js
+++ server.js
@@ -8,13 +8,30 @@ http.createServer(function (request, response) {
     var strUrl = 'http://api.lovebizhi.com/iphone_v3.php?a=search&kw='+arg.keyword+'&client_id=1002&model_id=100&screen_width='+arg.imgWidth+'&screen_height='+arg.imgHeight+'&bizhi_width='+arg.imgWidth+'&bizhi_height='+arg.imgHeight
     http.get(strUrl, function(res){
     	res.setEncoding("utf-8")
-    	var resData = []
+      var chunks = [];  
+      var size = 0;
     	res.on("data", function(chunk){
-       	resData.push(chunk)
+        chunks.push(chunk);  
+        size += chunk.length; 
     	})
     	res.on("end", function(){
+        var data = null;  
+        switch(chunks.length) {  
+          case 0: data = new Buffer(0);  
+           break;  
+          case 1: data = chunks[0];  
+            break;  
+          default:  
+            data = new Buffer(size);  
+            for (var i = 0, pos = 0, l = chunks.length; i < l; i++) {  
+              var chunk = chunks[i];  
+              chunk.copy(data, pos);  
+             pos += chunk.length;  
+            }  
+            break;  
+        }
         response.writeHead(200, {'Content-Type': 'text/json'})
-        response.write(resData.toString())
+        response.write(data.toString())
         response.end()
     	})
 	  })
