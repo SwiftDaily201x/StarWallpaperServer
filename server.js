02fb18386ddb9884f1840304c61654e90430b0b4
LuWeijun <luweijun@weidian.com>
2016-04-10 16:00:04 +0800

    回退；

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var http = require('http')
 var url = require("url")
 var fs = require('fs')
-var BufferHelper = require('bufferhelper');
 http.createServer(function (request, response) {
   var arg = url.parse(request.url,true).query
   if (arg.keyword && arg.imgWidth && arg.imgHeight) 
@@ -9,14 +8,13 @@ http.createServer(function (request, response) {
     var strUrl = 'http://api.lovebizhi.com/iphone_v3.php?a=search&kw='+arg.keyword+'&client_id=1002&model_id=100&screen_width='+arg.imgWidth+'&screen_height='+arg.imgHeight+'&bizhi_width='+arg.imgWidth+'&bizhi_height='+arg.imgHeight
     http.get(strUrl, function(res){
     	res.setEncoding("utf-8")
-    	var bufferHelper = new BufferHelper();
+    	var resData = []
     	res.on("data", function(chunk){
-       	bufferHelper.concat(chunk);
+       	resData.push(chunk)
     	})
     	res.on("end", function(){
-        var result = bufferHelper.toBuffer().toString();
         response.writeHead(200, {'Content-Type': 'text/json'})
-        response.write(result)
+        response.write(resData.toString())
         response.end()
     	})
 	  })
