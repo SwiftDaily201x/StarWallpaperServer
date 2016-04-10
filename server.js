ca02ef9a2317c2b741ed3dd5013e4350ff6b8fb7
LuWeijun <luweijun@weidian.com>
2016-04-10 16:41:04 +0800

    请求转发；

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-var http = require('http')
-var url = require("url")
-var fs = require('fs')
-http.createServer(function (request, response) {
-  var arg = url.parse(request.url,true).query
-  if (arg.keyword && arg.imgWidth && arg.imgHeight) 
-  {
-    var strUrl = 'http://api.lovebizhi.com/iphone_v3.php?a=search&kw='+arg.keyword+'&client_id=1002&model_id=100&screen_width='+arg.imgWidth+'&screen_height='+arg.imgHeight+'&bizhi_width='+arg.imgWidth+'&bizhi_height='+arg.imgHeight
-    http.get(strUrl, function(res){
-    	res.setEncoding("utf-8")
-    	var resData = []
-    	res.on("data", function(chunk){
-       	resData.push(chunk)
-    	})
-    	res.on("end", function(){
+var http = require('http');
+var app = http.createServer(function (req, res) {
+    var arg = url.parse(request.url,true).query
+      if (arg.keyword && arg.imgWidth && arg.imgHeight) 
+      {
+        var strUrl = '/iphone_v3.php?a=search&kw='+arg.keyword+'&client_id=1002&model_id=100&screen_width='+arg.imgWidth+'&screen_height='+arg.imgHeight+'&bizhi_width='+arg.imgWidth+'&bizhi_height='+arg.imgHeight
+        var sreq = http.request({
+            host:     'api.lovebizhi.com',
+            path:     strUrl,
+            method:   req.method
+        }, function(sres){
+            sres.pipe(res);
+            sres.on('end', function(){
+                console.log('done');
+            });
+        });
+        if (/POST|PUT/i.test(req.method)) {
+            req.pipe(sreq);
+        } else {
+            sreq.end();
+        }
+    } 
+    else
+    {
         response.writeHead(200, {'Content-Type': 'text/json'})
-        response.write(resData.toString())
+        response.write('You need keyword, imgWidth and imgHeight!')
         response.end()
-    	})
-	  })
-  }	
-  else
-  {
-    response.writeHead(200, {'Content-Type': 'text/json'})
-    response.write('You need keyword, imgWidth and imgHeight!')
-    response.end()
-  }
-}).listen(18080)
-console.log('GetAppIcon is running.')
+    }
+});
+app.listen(18080);
