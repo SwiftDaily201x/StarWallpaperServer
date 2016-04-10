625c640dc21c294b55b25c2ce40c88c9b2a9d6e9
LuWeijun <luweijun@weidian.com>
2016-04-10 15:30:57 +0800

    回退；

--- server.js
+++ server.js
@@ -8,30 +8,13 @@ http.createServer(function (request, response) {
     var strUrl = 'http://api.lovebizhi.com/iphone_v3.php?a=search&kw='+arg.keyword+'&client_id=1002&model_id=100&screen_width='+arg.imgWidth+'&screen_height='+arg.imgHeight+'&bizhi_width='+arg.imgWidth+'&bizhi_height='+arg.imgHeight
     http.get(strUrl, function(res){
     	res.setEncoding("utf-8")
-      var chunks = [];  
-      var size = 0;
+    	var resData = []
     	res.on("data", function(chunk){
-        chunks.push(chunk);  
-        size += chunk.length; 
+       	resData.push(chunk)
     	})
     	res.on("end", function(){
-        var data = null;  
-        switch(chunks.length) {  
-          case 0: data = new Buffer(0);  
-           break;  
-          case 1: data = chunks[0];  
-            break;  
-          default:  
-            data = new Buffer(size);  
-            for (var i = 0, pos = 0, l = chunks.length; i < l; i++) {  
-              var chunk = chunks[i];  
-              chunk.copy(data, pos);  
-             pos += chunk.length;  
-            }  
-            break;  
-        }
         response.writeHead(200, {'Content-Type': 'text/json'})
-        response.write(data.toString())
+        response.write(resData.toString())
         response.end()
     	})
 	  })
