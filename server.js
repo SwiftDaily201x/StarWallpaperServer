28b2b929de01c7ee5234acfab73538cb35476c2a
LuWeijun <luweijun@weidian.com>
2016-04-10 17:31:46 +0800

    修复“无数据”；

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var http = require('http');
+var url = require("url")
 var app = http.createServer(function (req, res) {
-    var arg = url.parse(request.url,true).query
-      if (arg.keyword && arg.imgWidth && arg.imgHeight) 
-      {
-        var strUrl = '/iphone_v3.php?a=search&kw='+arg.keyword+'&client_id=1002&model_id=100&screen_width='+arg.imgWidth+'&screen_height='+arg.imgHeight+'&bizhi_width='+arg.imgWidth+'&bizhi_height='+arg.imgHeight
-        var sreq = http.request({
+    var arg = url.parse(req.url,true).query
+    var strUrl = '/iphone_v3.php?a=search&kw='+arg.keyword+'&client_id=1002&model_id=100&screen_width='+arg.imgWidth+'&screen_height='+arg.imgHeight+'&bizhi_width='+arg.imgWidth+'&bizhi_height='+arg.imgHeight
+    var sreq = http.request({
             host:     'api.lovebizhi.com',
             path:     strUrl,
             method:   req.method
@@ -13,18 +12,11 @@ var app = http.createServer(function (req, res) {
             sres.on('end', function(){
                 console.log('done');
             });
-        });
-        if (/POST|PUT/i.test(req.method)) {
-            req.pipe(sreq);
-        } else {
-            sreq.end();
-        }
-    } 
-    else
-    {
-        response.writeHead(200, {'Content-Type': 'text/json'})
-        response.write('You need keyword, imgWidth and imgHeight!')
-        response.end()
+    });
+    if (/POST|PUT/i.test(req.method)) {
+        req.pipe(sreq);
+    } else {
+        sreq.end();
     }
 });
 app.listen(18080);
